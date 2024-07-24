9b4a368f45d20b1d465030fa5ad972c82b2e97d5
Rhaster <73538677+Rhaster@users.noreply.github.com>
2024-07-24 18:17:09 +0200

    Inicjalizacja

--- src/main/java/com/example/projektstartowy/model/User.java
+++ src/main/java/com/example/projektstartowy/model/User.java
@@ -8,20 +8,17 @@ import java.io.Serializable;
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(unique = true, nullable = false, updatable = false)
+    @Column(nullable = false, updatable = false)
     private Long id;
 
-    @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+
     private String email;
 
-    @Column(nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private AccountType accountType;
 
     public enum AccountType {
